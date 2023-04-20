5870006ac0ec43cb8d52732c8b15da56c5a58812
Paul Heymann <contact@paulheymann.de>
2023-04-20 18:15:27 +0200

    working sanity test suite

--- cmd/datacleanup/sanity_test.go
+++ cmd/datacleanup/sanity_test.go
@@ -19,14 +19,59 @@ func Test_SanityCheck(t *testing.T) {
 
 	financialPlanCity := decodeFp.DecodeFromJSON2(io.ReadCompleteFile(financialPlanCityFile))
 
-	department1 := financialPlanCity.Departments["1"]
-	checkBalance(t, department1.AdministrationBalance, department1.AdministrationBalance, "1", "Administration")
+	checkBalance(t, "City", "Administration", financialPlanCity.AdministrationBalance, model.Cashflow{
+		Total: map[model.BudgetYear]float64{
+			model.BudgetYear2022: -710_700.00,
+			model.BudgetYear2023: -1_779_500.00,
+			model.BudgetYear2024: -1_710_600.00,
+			model.BudgetYear2025: -1_273_200.00,
+			model.BudgetYear2026: -1_031_400.00,
+		},
+		Income: map[model.BudgetYear]float64{
+			model.BudgetYear2022: 70_385_700.00,
+			model.BudgetYear2023: 72_990_900.00,
+			model.BudgetYear2024: 74_673_000.00,
+			model.BudgetYear2025: 76_804_200.00,
+			model.BudgetYear2026: 77_902_600.00,
+		},
+		Expenses: map[model.BudgetYear]float64{
+			model.BudgetYear2022: -71_096_400.00,
+			model.BudgetYear2023: -74_770_400.00,
+			model.BudgetYear2024: -76_383_600.00,
+			model.BudgetYear2025: -78_077_400.00,
+			model.BudgetYear2026: -78_934_000.00,
+		},
+	})
+
+	checkBalance(t, "City", "Investments", financialPlanCity.InvestmentsBalance, model.Cashflow{
+		Total: map[model.BudgetYear]float64{
+			model.BudgetYear2022: 0.00,
+			model.BudgetYear2023: 0.00,
+			model.BudgetYear2024: -3_419_000.00,
+			model.BudgetYear2025: -2_134_100.00,
+			model.BudgetYear2026: -1_636_000.00,
+		},
+		Income: map[model.BudgetYear]float64{
+			model.BudgetYear2022: 12_955_700.00,
+			model.BudgetYear2023: 10_651_900.00,
+			model.BudgetYear2024: 8_571_800.00,
+			model.BudgetYear2025: 5_557_600.00,
+			model.BudgetYear2026: 5_557_500.00,
+		},
+		Expenses: map[model.BudgetYear]float64{
+			model.BudgetYear2022: -12_955_700.00,
+			model.BudgetYear2023: -10_651_900.00,
+			model.BudgetYear2024: -11_990_800.00,
+			model.BudgetYear2025: -7_691_700.00,
+			model.BudgetYear2026: -7_193_500.00,
+		},
+	})
 }
 
-func checkBalance(t *testing.T, expected model.Cashflow, actual model.Cashflow, department string, balance string) {
-	checkCashflowType(t, expected.Total, actual.Total, "Total", department, balance)
+func checkBalance(t *testing.T, department string, balance string, actual model.Cashflow, expected model.Cashflow) {
 	checkCashflowType(t, expected.Income, actual.Income, "Income", department, balance)
 	checkCashflowType(t, expected.Expenses, actual.Expenses, "Expenses", department, balance)
+	checkCashflowType(t, expected.Total, actual.Total, "Total", department, balance)
 }
 
 func checkCashflowType(
@@ -39,7 +84,7 @@ func checkCashflowType(
 ) {
 	for year, value := range expected {
 		if shared.IsUnequal(value, actual[year]) {
-			t.Errorf("%s.%s.%s: For year %s: expected=%f, got=%f", department, balance, cashflow, year, value, actual[year])
+			t.Errorf("%s.%s.%s: year %s with difference %.2f: expected=%f, got=%f", department, balance, cashflow, year, value-actual[year], value, actual[year])
 		}
 	}
 }
